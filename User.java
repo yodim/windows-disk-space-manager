f4c4e9c180582c2de318e8b5718fdad3dc03ecec
BATRONE MOHAMED EL MEHDI <81393311+medodim@users.noreply.github.com>
2021-05-11 17:51:54 +0000

    Update User.java

--- User.java
+++ User.java
@@ -9,21 +9,21 @@ public class User extends Personne {
 		String dir;
 		
 		System.out.println("Bienvenue "+username +" !");
-		System.out.println("Donnez le chemin du répertoire que vous voulez gérer \n"
+		System.out.println("Donnez le chemin du rÃ©pertoire que vous voulez gÃ©rer \n"
 							+ "par exemple : C://Users/username/Desktop/");
 		
-		dir = clavier.next();
+		dir = clavier.nextLine();
 		File directory = new File(dir);
 		
 		if (directory.isDirectory()) {
 			do
 	        {  System.out.println("\n\n1: Afficher les dossiers (nom + taille)");
 	           System.out.println("2: Afficher les fichiers (nom + taille)");
-	           System.out.println("3: Créer un dossier");
+	           System.out.println("3: CrÃ©er un dossier");
 	           System.out.println("4: Modifier fichier ou dossier");
 	           System.out.println("5: Supprimer dossier ou fichier");
 	           System.out.println("6: Quitter l'application");
-	           System.out.println("\n\nEntrer le numéro de l'opération de votre choix");
+	           System.out.println("\n\nEntrer le numÃ©ro de l'opÃ©ration de votre choix");
 	           
 	           choix=clavier.nextInt();
 	           Dossier dossier = new Dossier();
@@ -68,7 +68,7 @@ public class User extends Personne {
 	            		  fich.delete(dir);
 	            		 break;
 	            	  case 2:
-	            		  System.out.println("Entrez le nom du dossier à supprimer :");
+	            		  System.out.println("Entrez le nom du dossier Ã  supprimer :");
 	            		  String fold_name = clavier.next();
 	            		  dossier.delete(dir, fold_name);
 	            		 break;            		 
@@ -77,7 +77,7 @@ public class User extends Personne {
 	                 
 	              case 6 : System.out.println("FIN");
 	                 break;
-	              default : System.out.println("Choix erroné");
+	              default : System.out.println("Choix erronÃ©");
 	           }
 	        }while(choix !=6);
 	        clavier.close();            
